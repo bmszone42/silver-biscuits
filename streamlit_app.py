154f129fb00fbf1833991d2a293d9920e40881d1
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-06-03 16:52:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from pptx import Presentation
 import base64
 import openai
 from collections.abc import Iterable
-from tiktoken import Tokenizer
+from tiktoken.Tokenizer import Tokenizer
 
 MAX_TOKENS = 4096  # Maximum tokens allowed in a single API call
 TOKENS_PER_SLIDE_ESTIMATE = 100  # Rough estimate of tokens used per slide
